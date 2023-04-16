a72891d8d0c99c31c88297d0175e6c7c8f879a00
Rennie deGraaf <rennie.degraaf@gmail.com>
2023-04-16 13:36:59 -0700

    LibreJS support
    *   www/dyphal.js: added license tags

--- www/dyphal.js
+++ www/dyphal.js
@@ -1,3 +1,4 @@
+/*  @license magnet:?xt=urn:btih:cf05388f2679ee054f2beb29a391d25f4e673ac3&dn=gpl-2.0.txt GPL-2.0 */
 /**
   Scripts for Dyphal, the Dynamic Photo Album.
   Copyright (c) Rennie deGraaf, 2005-2023.
@@ -970,3 +971,4 @@ log(navigator.userAgent);
 log(window.innerWidth + "x" + window.innerHeight);
 
 }());
+/* @license-end */
